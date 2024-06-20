dc7d6acc8c5f5c100dfe85824624e78b49268957
Apoorv Gusain <apoorvgusain123@gmail.com>
2024-06-20 15:30:23 +0530

    final changes

---
+++ components/LoginForm.jsx
@@ -0,0 +1,69 @@
+"use client";
+
+import Link from "next/link";
+import { useState } from "react";
+import { signIn } from "next-auth/react";
+import { useRouter } from "next/navigation";
+
+export default function LoginForm() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const router = useRouter();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const res = await signIn("credentials", {
+        email,
+        password,
+        redirect: false,
+      });
+
+      if (res.error) {
+        setError("Invalid Credentials");
+        return;
+      }
+
+      router.replace("dashboard");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  return (
+    <div className="grid place-items-center h-screen">
+      <div className="shadow-lg p-5 rounded-lg border-y-4  border-blue-700 ">
+        <h1 className="text-xl font-bold my-4 text-center">Login</h1>
+
+        <form className="flex flex-col gap-3" onSubmit={handleSubmit}>
+          <input
+            className="my-1 py-2 px-2"
+            onChange={(e) => setEmail(e.target.value)}
+            type="text"
+            placeholder="Email"
+          />
+          <input
+            className="my-1  py-2 px-2"
+            onChange={(e) => setPassword(e.target.value)}
+            type="password"
+            placeholder="Password"
+          />
+          <button className="bg-blue-600 text-white font-bold cursor-pointer px-6 py-2 rounded-xl">
+            Login
+          </button>
+          {error && (
+            <div className="bg-red-500 text-white w-fit text-sm py-1 px-3 rounded-md mt-2">
+              {error}
+            </div>
+          )}
+          <Link className="text-sm mt-3 text-center" href={"/register"}>
+            Don&apos;t have an account?{" "}
+            <span className="underline">Register</span>
+          </Link>
+        </form>
+      </div>
+    </div>
+  );
+}
